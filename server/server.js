678edcce3aadec6a59cbae90bd140e9def69187b
jaaziel.figueroa <180183@utags.edu.mx>
2021-04-14 15:05:58 -0500

    instalciones y coneccion con base de datos

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.send('<h1>Bienvenido a mi servidor REST del proyecto final</h1>');
+});
+
+
+
+mongoose.connect('mongodb+srv://admin:password_server20@cluster0.nckh7.mongodb.net/ProyectoFinal', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('servidor en linea en el puerto: ', process.env.PORT);
+});
